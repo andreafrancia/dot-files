032099d0d0b8f60f868ec4c2090fe5d82d1a39aa
Andrea Francia <andrea@andreafrancia.it>
2011-10-03 15:21:07 +0200

    Refactor: Now svn-repair-move is a script.

--- bash-interactive.d/50-svn-repair-move.bash
+++
@@ -1,9 +0,0 @@
-function svn-repair-move() {
-    usage="Usage:
-    $FUNCNAME SRC DEST"
-    local src="${1?"$usage"}"
-    local dest="${2?"$usage"}"
-    [ -f "$src" ] && { echo "Refusing overwriting \`$src'" >&2 ; return; }
-    mv --no-clobber "$dest" "$src"
-    svn mv "$src" "$dest"
-}
