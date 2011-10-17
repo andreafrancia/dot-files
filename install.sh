b44839bb2663a15f45e224e81fc6eaa41900667d
Andrea Francia <andrea@andreafrancia.it>
2011-10-17 16:56:16 +0200

    Added a README and install script.

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set errexit
+set nounset
+
+script_dir="$(dirname "$0")"
+
+main() {
+
+    items=( .bash_profile .bashrc 
+            .cvsignore 
+            .git-prompt.conf .gitconfig .gitignore .gitmodules 
+            .gvimrc .vim .vimrc 
+            bin git-prompt )
+
+    files_to_remove=()
+    for item in "${items[@]}"; do
+        if exists_and_is_not_a_link ~/"$item"; then
+	    files_to_remove=( "${files_to_remove[@]}" "~/$item" )
+            echo "${files_to_remove[@]}"
+	fi  
+    done
+
+    if [ ${#files_to_remove[@]} -gt 0 ]; then
+	die "Failed: Please remove those files before the installation: ${files_to_remove[*]}"
+    fi
+
+    for item in "${items[@]}"; do
+        ln -sfTv "$(item_abspath "$item")" ~/"$item"
+    done
+}
+
+item_abspath() {
+    local item="$1"
+    local item_relpath="$script_dir/$item"
+    local item_abspath="$(readlink -f "$item_relpath")"
+    echo "$item_abspath"
+}
+
+exists_and_is_not_a_link() {
+    local path="$1"
+   
+    [ -e "$path" -a ! -L "$path" ]
+}
+
+die() {
+    local message="$1"
+    echo "$message" >&2
+    exit 1
+}
+
+main
