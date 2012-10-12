7789ef7044987778251ec6d5e37f0946658412c9
Andrea Francia <andrea@andreafrancia.it>
2012-10-12 14:03:44 +0200

    Made script for installing brew

---
+++ install-python.sh
@@ -0,0 +1,6 @@
+easy_install pip
+pip install --upgrade distribute
+pip install pyflakes            # needed by syntastic
+pip install readline ipython    # install them toghether in order to avoid buggy readline of OS X
+pip install nose
+easy_install nose-machineout    # as of 2011-11-02 machine-out seems not working with pip
---
+++ install-useful-packages-from-brew.sh
@@ -0,0 +1,39 @@
+brew install findutils 
+brew install xz
+brew install coreutils
+brew install a2ps
+brew install ack
+brew install colordiff
+brew install ctags 
+brew install curl
+brew install dos2unix
+brew install git
+brew install grep
+brew install htop
+brew install nmap
+brew install par
+brew install subversion
+brew install unrar
+brew install watch
+brew install wget 
+brew install python
+brew install bash
+brew install bash-completion
+brew install grep
+
+# boost bash completion
+curl http://worksintheory.org/files/misc/bash_completion_svn -o "$(brew --prefix)/etc/bash_completion.d/bash_completion_svn"
+
+# Install MacVim
+brew install macvim --enable-cscope --enable-clipboard \
+                    --custom-icons --with-envycoder --override-system-vim 
+mkdir -p ~/Applications
+brew linkapps
+
+# OSX already ships the GNU grep, the version it provides is 2.5.1. The
+# essential option --ignore-dir has been introduced in 2.5.1a.
+brew install https://raw.github.com/adamv/homebrew-alt/master/duplicates/grep.rb
+
+# Tell OSX to show full path in Finder title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+killall Finder
