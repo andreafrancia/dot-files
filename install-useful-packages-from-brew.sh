4104fc765a42730457e546c3b6c089fc7808b6e8
Andrea Francia <andrea@andreafrancia.it>
2013-01-09 17:59:02 +0100

    Removed duplicate grep, added md5sum

--- install-useful-packages-from-brew.sh
+++ install-useful-packages-from-brew.sh
@@ -21,6 +21,7 @@ brew install bash-completion
 brew install grep
 brew install openssh
 brew install ssh-copy-id
+brew install md5sha1sum
 
 # boost bash completion
 curl http://worksintheory.org/files/misc/bash_completion_svn -o "$(brew --prefix)/etc/bash_completion.d/bash_completion_svn"
@@ -30,10 +31,6 @@ brew install macvim --enable-cscope --enable-clipboard \
                     --custom-icons --with-envycoder --override-system-vim 
 mkdir -p ~/Applications
 brew linkapps
-
-# OSX already ships the GNU grep, the version it provides is 2.5.1. The
-# essential option --ignore-dir has been introduced in 2.5.1a.
-brew install https://raw.github.com/adamv/homebrew-alt/master/duplicates/grep.rb
 
 # Tell OSX to show full path in Finder title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
