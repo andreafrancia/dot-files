5df72ec259a43bd461ae47d0b8985f7f0a7c3c63
Andrea Francia <andrea@andreafrancia.it>
2017-03-11 21:15:48 +0100

    Removed unuseful line

--- .aliases.sh
+++ .aliases.sh
@@ -3,5 +3,3 @@ alias a='git add -A . && git status --short'
 alias c='git commit -m "Save." .'
 alias d='git diff HEAD'
 alias s='git status --short'
-
-[ -s ~/.bashrc.local ] && source ~/.bashrc.local
