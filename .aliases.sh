3e4b6806bf102fe33435aa90c8745cc4a454efd9
Andrea Francia <andrea@andreafrancia.it>
2017-03-11 21:19:05 +0100

    Fix loading of local aliases

--- .aliases.sh
+++ .aliases.sh
@@ -3,3 +3,5 @@ alias a='git add -A . && git status --short'
 alias c='git commit -m "Save." .'
 alias d='git diff HEAD'
 alias s='git status --short'
+
+[ -s ~/.aliases.sh.local ] && source ~/.aliases.sh.local
