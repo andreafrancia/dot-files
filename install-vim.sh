69dd064577c4e1baa92100ae3fa1eff4b5f5cc51
Andrea Francia <andrea@andreafrancia.it>
2022-04-03 19:08:48 +0200

    Update install-vim.sh

--- install-vim.sh
+++ install-vim.sh
@@ -3,7 +3,3 @@ brew install macvim --enable-cscope --enable-clipboard \
                     --custom-icons --with-envycoder --override-system-vim 
 mkdir -p ~/Applications
 brew linkapps
-
-# Tell OSX to show full path in Finder title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
-killall Finder
